ed01e44c5153accda5001697f700ec68de6a4ace
Glitch (ict-skills-assignment-1-sc) <none>
2020-08-05 13:23:04 +0000

    🚏🎰 Checkpoint ./models/assessment-store.json:17974984/242 ./models/assessment-store.js:17974984/343 ./views/about.hbs:17974984/57

--- models/assessment-store.js
+++ models/assessment-store.js
@@ -2,25 +2,6 @@
 
 const logger = require('../utils/logger');
 
-const assessment1 = {
-  weight: "70",
-  chest: "30",
-  thigh: "40",
-  upperarm: "20",
-  waist: "50",
-  hips: "40",
-  trend: "false"
-}
+const assessments = require('./assessment-store.json').assessments
 
-const assessment2 = {
-  weight: "80",
-  chest: "35",
-  thigh: "42",
-  upperarm: "28",
-  waist: "55",
-  hips: "45",
-  trend: "true"
-}
-
-const assessments = [assessment1, assessment2];
 module.exports = assessments;
