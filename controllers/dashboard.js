db10f4c05a32b8cf30d065a0c97fc282000b153a
Glitch (ict-skills-assignment-1-sc) <none>
2020-08-08 14:22:57 +0000

    👨🎎 Checkpoint ./views/partials/memberinfo.hbs:17974984/7 ./controllers/dashboard.js:17974984/88 ./models/member-store.json:17974984/25

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,13 +2,15 @@
 
 const logger = require("../utils/logger");
 const assessments = require("../models/assessment-store.js")
+const members = require("../modles/member-store.js")
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "Dashboard",
-      assessment: assessments
+      assessment: assessments,
+      member: members
     };
     logger.info('about to render', assessments)
     response.render("dashboard", viewData);
