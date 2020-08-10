a0a5b59b6b0a729ad72e948b31a431e281b2858e
Glitch (ict-skills-assignment-1-sc) <none>
2020-08-10 15:39:59 +0000

    📷🎍 Checkpoint ./routes.js:17974984/9 ./views/partials/listassessments.hbs:17974984/33

--- routes.js
+++ routes.js
@@ -7,9 +7,11 @@ const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 
 router.get("/", dashboard.index);
+
 router.get("/dashboard", dashboard.index);
 router.post("/dashboard/addassessment", dashboard.addAssessment);
-router.get("/dashboard/deletesassessment", dashboard.deleteAssessment);
+router.get("/dashboard/deleteassessment/:id", dashboard.deleteAssessment);
+
 router.get("/about", about.index);
 
 
