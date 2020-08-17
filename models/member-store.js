0755fcc2125c98f0ae9e08df5614cdced1a08ff6
Glitch (ict-skills-assignment-1-sc) <none>
2020-08-17 16:23:39 +0000

    ⛵️🕶 Checkpoint ./models/member-store.json:17974984/460 ./models/assessment-store.js:17974984/16 ./models/member-store.js:17974984/540

--- models/member-store.js
+++ models/member-store.js
@@ -33,6 +33,29 @@ const memberStore = {
     this.store.remove(this.collection, member);
     this.store.save();
   },
+  
+    getAllAssessments() {
+    return this.store.findAll(this.collection);
+  },
+
+  getAssessment(id) {
+    return this.store.findOneBy(this.collection, { id: id });
+  },
+  
+  getUserAssessments(userid) {
+    return this.store.findBy(this.collection, { userid: userid });
+  },
+  
+  addAssessment(assessment) {
+    this.store.add(this.collection, assessment);
+    this.store.save();
+  },
+
+  deleteAssessment(id) {
+    const assessment = this.getAssessment(id);
+    this.store.remove(this.collection, assessment);
+    this.store.save();
+  }
 };
 
 
