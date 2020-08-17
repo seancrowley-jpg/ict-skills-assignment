fa6baf49e3cedb453b28b10f7c866de61714e80e
Glitch (ict-skills-assignment-1-sc) <none>
2020-08-17 12:32:02 +0000

    🌺💏 Checkpoint ./models/trainer-store.js:17974984/396 ./views/partials/memberinfo.hbs:17974984/1741 ./views/dashboard.hbs:17974984/19

--- models/trainer-store.js
+++ models/trainer-store.js
@@ -4,5 +4,16 @@ const _ = require('lodash');
 const JsonStore = require('./json-store');
 
 const trainerStore = {
+  store: new JsonStore('./models/trainer-store.json', { trainers: []}),
+  collection: 'trainers',
   
-}
+  getTrainerByEmail(email) {
+    return this.store.findOneBy(this.collection, { email: email });
+  },
+  
+  getTrainerByPassword(password) {
+    return this.store.findOneBy(this.collection, { password: password });
+  },
+};
+
+module.exports = trainerStore;
