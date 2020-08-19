a48f59b2950612282d47f1b472bb44971c28729b
Glitch (ict-skills-assignment-1-sc) <none>
2020-08-19 17:01:39 +0000

    💑🍂 Checkpoint ./controllers/dashboard.js:17974984/22 ./utils/member-stats.js:17974984/511

--- utils/member-stats.js
+++ utils/member-stats.js
@@ -4,7 +4,26 @@ const memberStats = {
   bmi: undefined,
   bmiCategory: undefined,
   isidelabodyweight: undefined,
-  trend: undefined
+  trend: undefined,
+  
+  generateMemberStats(member)
+  {
+    const weight = member.startingweight;
+    if(member.assessments > 0)
+      {
+        const assessment = member.assessments.get(member.assessments.length -1);
+        weight = assessment.weight;
+      }
+    memberStats.bmi = this.calculateBMI(member,weight)
+  },
+  
+  calculateBMI (member,weight)
+  {
+    if(member.height <= 0)
+      return 0;
+    else
+      return Math.round(weight / (member.height * member.height));
+  }
 }
 
 module.exports = memberStats;
