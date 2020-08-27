ba8438e8f847f6c0f96d08b8650352ad1698328c
Glitch (ict-skills-assignment-1-sc) <none>
2020-08-27 15:08:24 +0000

    🏠🌪 Checkpoint ./controllers/comment.js:17974984/1100 ./views/comment.hbs:17974984/434 ./views/partials/listtrainerassessments.hbs:17974984/562 ./routes.js:17974984/73

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,5 +1,35 @@
+"use strict";
+
 const logger = require("../utils/logger");
 const accounts = require ('./accounts.js');
 const memberStore = require("../models/member-store.js")
 const analytics = require("../utils/analytics.js")
 const uuid = require("uuid");
+
+const comment = {
+  index(request,response)
+  {
+    const memberId = request.params.id
+    const assessmentId = request.params.assessment.id
+    logger.debug(`Editing assessment ${assessmentId} from Member ${memberId}`);
+    const viewData = {
+      member: memberStore.getMember(memberId),
+      assessment: memberStore.getAssessment(memberId,assessmentId)
+    };
+    response.render("comment")
+  },
+  
+  updateComment(request, response)
+  {
+    const assessmentId = request.params.assessmentid;
+    const userId =request.params.userid;
+    const member = memberStore.getMember(userId);
+    //const assessment = memberStore.getAssessment(memberId,assessmentId)
+    //logger.info("Assessment",assessment)
+    const comment = request.body.comment;
+    //logger.debug("Updating Comment ${assessmentId} from Member ${memberId}");
+    logger.info("Comment = ",comment)
+    memberStore.editComment(assessmentId,comment);
+    response.redirect("/trainerdashboard");
+  },
+};
--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 const trainerdashboard = require("./controllers/trainerdashboard.js")
 const settings = require("./controllers/settings.js")
+const comment = require("./controllers/comment.js");
 
 router.get('/', accounts.index);
 router.get('/login', accounts.login);
@@ -23,7 +24,9 @@ router.get("/dashboard/deleteassessment/:assessmentid", dashboard.deleteAssessme
 router.get("/trainerdashboard", trainerdashboard.index);
 router.get("/trainerassessment/:id", trainerdashboard.trainerAssessment);
 router.get("/trainerdashboard/deletemember/:id", trainerdashboard.deleteMember);
-router.post("/trainerassessment/:id/addcomment", trainerdashboard.updateComment);
+//router.post("/trainerassessment/:id/addcomment", trainerdashboard.updateComment);
+
+
 
 router.get('/settings', settings.index);
 router.post('/settings', settings.updateMember);
