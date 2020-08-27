f3b4bf7c71b5ca523b64c18bb95a82285215f5ae
Glitch (ict-skills-assignment-1-sc) <none>
2020-08-27 16:43:40 +0000

    👆🐊 Checkpoint ./controllers/comment.js:17974984/531

--- controllers/comment.js
+++ controllers/comment.js
@@ -23,16 +23,13 @@ const comment = {
   
   updateComment(request, response)
   {
-    const assessmentId = request.params.assessmentid;
-    const userId =request.params.userid;
-    const member = memberStore.getMember(userId);
-    //const assessment = memberStore.getAssessment(memberId,assessmentId)
-    //logger.info("Assessment",assessment)
+    const memberId = request.params.id
+    const member = memberStore.getMember(memberId)
+    const assessmentId = request.params.assessmentid
     const comment = request.body.comment;
-    //logger.debug("Updating Comment ${assessmentId} from Member ${memberId}");
     logger.info("Comment = ",comment)
-    memberStore.editComment(assessmentId,comment);
-    response.redirect("/trainerdashboard");
+    memberStore.editComment(memberId,assessmentId,comment);
+    response.redirect("/trainerassessment/:id");
   },
 };
 
